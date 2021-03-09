cf2454ad5b36d9173d9f7ad107733b982dbcb44e
Andrew Jayasinghe <heshronferdi@gmail.com>
2021-03-09 12:52:11 -0800

    Update app.py

--- reciever/app.py
+++ reciever/app.py
@@ -1,133 +1,158 @@
-import datetime
-import json
-import logging.config
-from pykafka import KafkaClient
-from pykafka.common import OffsetType
+# import datetime
+# import json
+# import logging.config
+# from pykafka import KafkaClient
+# from pykafka.common import OffsetType
+# import connexion
+# import yaml
+# from connexion import NoContent
+# import requests
+
+
+# MAX_EVENTS = 10
+# with open('app_conf.yml', 'r') as f:
+#     app_config = yaml.safe_load(f.read())
+
+# # GROCERY_URL = app_config["Groceries"]["url"]
+# # PRODUCE_URL = app_config["Produce"]["url"]
+# HOST = app_config["events"]["hostname"]
+# PORT = app_config["events"]["port"]
+
+# with open('log_conf.yml', 'r') as f:
+#     log_config = yaml.safe_load(f.read())
+#     logging.config.dictConfig(log_config)
+
+# logger = logging.getLogger('basicLogger')
+
+# def grocery_item_scan(body):
+#     '''adds grocery item to inventory'''
+#     hostname = "%s:%d" % (HOST, PORT)
+#     client = KafkaClient(hosts=hostname)
+#     topic = client.topics[str.encode(app_config["events"]["topics"])]
+#     producer = topic.get_sync_producer()
+#     msg = { "type":"gc",
+#             "datetime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+#             "payload": body}
+
+#     msg_str = json.dumps(msg)
+#     producer.produce(msg_str.encode('utf-8'))
+
+#     logger.info("Returned event grocery_scan " + str(body["id"]) + " with status 201")
+
+#     return NoContent, 201
+
+
+# def produce_item_scan(body):
+#     '''adds produce items to inventory'''
+#     hostname = "%s:%d" % (HOST, PORT)
+#     client = KafkaClient(hosts=hostname)
+#     topic = client.topics[str.encode(app_config["events"]["topics"])]
+#     producer = topic.get_sync_producer()
+#     msg = {"type": "pr",
+#            "datetime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+#            "payload": body}
+
+#     msg_str = json.dumps(msg)
+#     producer.produce(msg_str.encode('utf-8'))
+
+#     logger.info("Returned event grocery_scan " + str(body["id"]) + " with status 201")
+
+#     return NoContent, 201
+
+# app = connexion.FlaskApp(__name__, specification_dir='')
+# app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+
+
+# if __name__ == "__main__":
+#     app.run(port=8080)
+
 import connexion
-import yaml
 from connexion import NoContent
+import json
 import requests
+import yaml
+import logging
+import logging.config
+import datetime
+from pykafka import KafkaClient
+import os
 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
-MAX_EVENTS = 10
-with open('app_conf.yml', 'r') as f:
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-# GROCERY_URL = app_config["Groceries"]["url"]
-# PRODUCE_URL = app_config["Produce"]["url"]
-HOST = app_config["events"]["hostname"]
-PORT = app_config["events"]["port"]
-
-with open('log_conf.yml', 'r') as f:
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
+
 logger = logging.getLogger('basicLogger')
 
-def grocery_item_scan(body):
-    '''adds grocery item to inventory'''
-    hostname = "%s:%d" % (HOST, PORT)
-    client = KafkaClient(hosts=hostname)
-    topic = client.topics[str.encode(app_config["events"]["topics"])]
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
+AIR_EVENT = 'Air Reading'
+ENV_EVENT = 'Env. Reading'
+
+kafka_server = app_config['events']['hostname']
+kafka_port = app_config['events']['port']
+kafka_topic = app_config['events']['topic']
+
+
+
+def measure_air_quality(body):
+    """Takes in an air reading, and sends to Data storage service"""
+    logger.info(f'Received event {AIR_EVENT} request with unique id ({body["sensor_id"]})')
+    # response = requests.post(app_config['air_reading']['url'], json.dumps(body),
+    #                          headers={'content-type': 'application/json'})
+    client = KafkaClient(hosts=f'{kafka_server}:{kafka_port}')
+    topic = client.topics[kafka_topic]
     producer = topic.get_sync_producer()
-    msg = { "type":"gc",
-            "datetime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
-            "payload": body}
-
-    msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
-
-    logger.info("Returned event grocery_scan " + str(body["id"]) + " with status 201")
-
-    return NoContent, 201
-
-
-def produce_item_scan(body):
-    '''adds produce items to inventory'''
-    hostname = "%s:%d" % (HOST, PORT)
-    client = KafkaClient(hosts=hostname)
-    topic = client.topics[str.encode(app_config["events"]["topics"])]
-    producer = topic.get_sync_producer()
-    msg = {"type": "pr",
-           "datetime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+    msg = {"type": "air_reading",
+           "datetime":
+               datetime.datetime.now().strftime(
+                   "%Y-%m-%dT%H:%M:%S"),
            "payload": body}
-
     msg_str = json.dumps(msg)
     producer.produce(msg_str.encode('utf-8'))
-
-    logger.info("Returned event grocery_scan " + str(body["id"]) + " with status 201")
-
+    logger.info(f'Returned event {AIR_EVENT} response id ({body["sensor_id"]}) with status code 201  ')
     return NoContent, 201
 
 
-# def get_grocery_scan(index):
-#     """ Gets new grocery scan readings after the timestamp """
-#     hostname = "%s:%d" % (HOST, PORT)
-#     client = KafkaClient(hosts=hostname)
-#     topic = client.topics[str.encode(app_config["events"]["topics"])]
-#     consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
-#
-#     logger.info("Retrieving reading at index %d" % index)
-#
-#     count = 0
-#     reading = None
-#
-#     try:
-#         for msg in consumer:
-#             msg_str = msg.value.decode('utf-8')
-#             msg = json.loads(msg_str)
-#
-#             if msg["type"] == "gc":
-#
-#                 if count == index:
-#                     reading = msg["payload"]
-#                     return reading, 200
-#
-#                 count += 1
-#
-#     except:
-#         logger.error("No more messages found")
-#
-#     logger.error("could not find gc at index %d" %index)
-#
-#     return {"message":"Not Found"}, 404
-#
-# def get_produce_scan(index):
-#     """ Gets new produce scan readings after the timestamp """
-#     hostname = "%s:%d" % (HOST, PORT)
-#     client = KafkaClient(hosts=hostname)
-#     topic = client.topics[str.encode(app_config["events"]["topics"])]
-#     consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
-#
-#     logger.info("Retrieving reading at index %d" % index)
-#
-#     count = 0
-#     reading = None
-#
-#     try:
-#         for msg in consumer:
-#             msg_str = msg.value.decode('utf-8')
-#             msg = json.loads(msg_str)
-#
-#             if msg["type"] == "pr":
-#
-#                 if count == index:
-#                     reading = msg["payload"]
-#                     return reading, 200
-#
-#                 count += 1
-#
-#     except:
-#         logger.error("No more messages found")
-#
-#     logger.error("could not find pr at index %d" % index)
-#
-#     return {"message": "Not Found"}, 404
-#
+def measure_environment(body):
+    """Takes in an environment reading, and sends to Data storage service"""
+    logger.info(f'Received event {ENV_EVENT} request with unique id({body["sensor_id"]})')
+    # response = requests.post(app_config['env_reading']['url'], json.dumps(body),
+    #                          headers={'content-type': 'application/json'})
+    # logger.info(f'Returned event {ENV_EVENT} response id ({body["sensor_id"]}) with status code {response.status_code}  ')
+    hostname = "%s:%d" % (app_config["events"]["hostname"],
+                          app_config["events"]["port"])
+    client = KafkaClient(hosts=f'{kafka_server}:{kafka_port}')
+    topic = client.topics[kafka_topic]
+    producer = topic.get_sync_producer()
+    msg = {"type": "env_reading",
+           "datetime":
+               datetime.datetime.now().strftime(
+                   "%Y-%m-%dT%H:%M:%S"),
+           "payload": body}
+    msg_str = json.dumps(msg)
+    logger.info("Message: %s" % msg)
+    producer.produce(msg_str.encode('utf-8'))
+    return NoContent, 201
+
+
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
     app.run(port=8080)
-
