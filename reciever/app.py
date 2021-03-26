c58ac3fbf2f674b891289a1d2b4a83871406849b
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2021-03-25 18:39:07 -0700

    Update app.py

--- reciever/app.py
+++ reciever/app.py
@@ -7,6 +7,7 @@ import connexion
 import yaml
 from connexion import NoContent
 import requests
+import os
 
 
 MAX_EVENTS = 10
@@ -18,11 +19,28 @@ with open('app_conf.yml', 'r') as f:
 HOST = app_config["events"]["hostname"]
 PORT = app_config["events"]["port"]
 
-with open('log_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+    
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
-
+    
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file) 
+logger.info("Log Conf File: %s" % log_conf_file)
+
 
 def grocery_item_scan(body):
     '''adds grocery item to inventory'''
